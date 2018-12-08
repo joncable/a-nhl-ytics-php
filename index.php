837f2457ae272d45e21f07f21f8c7fd0e94cb24c
Coding <Coding@Jonathans-MacBook-Pro.local>
2018-12-08 01:24:02 -0800

    Query database for player_ids for a given team

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
 <?php
 
-# This function reads your DATABASE_URL config var and returns a connection
-# string suitable for pg_connect. Put this in your app.
+// This function reads your DATABASE_URL config var and returns a connection
+// string suitable for pg_connect. Put this in your app.
 function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["DATABASE_URL"]));
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+    extract(parse_url($_ENV["DATABASE_URL"]));
+    return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
 
-# Here we establish the connection. Yes, that's all.
-$pg_conn = pg_connect(pg_connection_string_from_database_url());
+function get_team_lines($team_id) {
+    // Establish database connection
+    $pg_conn = pg_connect(pg_connection_string_from_database_url());
 
-# Now let's use the connection for something silly just to prove it works:
-$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+    // Now let's use the connection for something silly just to prove it works:
+    $result = pg_query($pg_conn, "SELECT player_id FROM lines WHERE team_id=${team_id}");
+
+    return $result;
+}
+
+$result = get_team_lines(3);
 
 print "<pre>\n";
 
 if (!pg_num_rows($result)) {
   print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
 } else {
-  print "Tables in your database:\n";
+  print "Player ids for team_id=${team_id}:\n";
   while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
 }
 
