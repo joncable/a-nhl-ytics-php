43a150045007c29b02952bb489fff9d746f0659c
Coding <Coding@Jonathans-MacBook-Pro.local>
2018-12-09 13:56:58 -0800

    Add team ids to the lines dropdown

--- index.php
+++ index.php
@@ -21,41 +21,6 @@
     <p class="lead">Computed NHL Lines calculated based on their ice time together in most recent game</p>
 </div>
 
-<div class="container">
-    <nav id="myNavbar" class="navbar navbar-default" role="navigation">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">NHLytics</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li class="dropdown">
-                        <a href="#" data-toggle="dropdown" class="dropdown-toggle">Lines <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Inbox</a></li>
-                            <li><a href="#">Drafts</a></li>
-                            <li><a href="#">Sent Items</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#">Trash</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </div>
-    </nav>
-</div>
-
-<div class="container">
 
 <?php
 
@@ -84,6 +49,61 @@ function get_team_lines($team_id) {
 
     return $lines;
 }
+
+function get_team_ids() {
+    // Establish database connection
+    $pg_conn = pg_connect(pg_connection_string_from_database_url());
+
+    // Now let's use the connection for something silly just to prove it works:
+    $result = pg_query($pg_conn, "SELECT DISTINCT team_id FROM lines");
+
+    $team_ids = ();
+    while ($row = pg_fetch_row($result)) {
+        $team_ids[] = $row[0];
+     }
+
+    return $team_ids;
+}
+
+?>
+
+<div class="container">
+    <nav id="myNavbar" class="navbar navbar-default" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">NHLytics</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li class="dropdown">
+                        <a href="#" data-toggle="dropdown" class="dropdown-toggle">Lines <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <?php
+                                foreach ($team_ids as $team_id) {
+                                    echo "<li><a href='?team_id=${team_id}'>Team $team_id</a></li>";
+                                }
+                            ?>
+                        </ul>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div>
+    </nav>
+</div>
+
+<div class="container">
+
+<?php
 
 $team_id = $_GET['team_id'];
 
