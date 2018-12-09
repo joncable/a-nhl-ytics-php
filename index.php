f9275d8fa59f0722c03e1ae29e6135329364087b
Coding <Coding@Jonathans-MacBook-Pro.local>
2018-12-09 02:22:30 -0800

    Remove navbar on the right

--- index.php
+++ index.php
@@ -50,17 +50,6 @@
                         </ul>
                     </li>
                 </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="dropdown">
-                        <a href="#" data-toggle="dropdown" class="dropdown-toggle">Stats <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Action</a></li>
-                            <li><a href="#">Another action</a></li>
-                            <li class="divider"></li>
-                            <li><a href="#">Settings</a></li>
-                        </ul>
-                    </li>
-                </ul>
             </div><!-- /.navbar-collapse -->
         </div>
     </nav>
