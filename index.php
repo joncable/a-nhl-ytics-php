4e9b00167ed490fb765313dc1e07eca7e6601519
Coding <Coding@Jonathans-MacBook-Pro.local>
2018-12-09 01:57:15 -0800

    Add back NHL lines

--- index.php
+++ index.php
@@ -59,5 +59,59 @@
         </div>
     </nav>
 </div>
+
+<div class="container">
+
+<?php
+
+// This function reads your DATABASE_URL config var and returns a connection
+// string suitable for pg_connect. Put this in your app.
+function pg_connection_string_from_database_url() {
+    extract(parse_url($_ENV["DATABASE_URL"]));
+    return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+
+function get_team_lines($team_id) {
+    // Establish database connection
+    $pg_conn = pg_connect(pg_connection_string_from_database_url());
+
+    // Now let's use the connection for something silly just to prove it works:
+    $result = pg_query($pg_conn, "SELECT depth,player_id,position FROM lines WHERE team_id=${team_id}");
+
+    $lines = [];
+    while ($row = pg_fetch_row($result)) {
+        $depth = $row[0];
+        $player_id = $row[1];
+        $position = $row[2] == 'D' ? 'D' : 'F';
+
+        $lines[$position][$depth][] = $player_id;
+     }
+
+    return $lines;
+}
+
+$lines = get_team_lines(5);
+
+foreach ($lines as $position => $depths) {
+    echo '<table class="table">';
+    if ($position == 'F') {
+        echo "<tr><th>Left Wing</th><th>Center</th><th>Right Wing<th></tr>";
+    } else if ($position == 'D') {
+        echo "<tr><th>Defense</th></tr>";
+    }
+    foreach ($depths as $depth => $players) {
+        echo "<tr>";
+        foreach ($players as $player_id) {
+            echo "<td>${player_id}</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+}
+
+?>
+
+</div> <!-- /container -->
+
 </body>
-</html>    
+</html>
