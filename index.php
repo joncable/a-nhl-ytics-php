311d98d3610dd5bb26f7d850efcaf795c9119052
Coding <Coding@Jonathans-MacBook-Pro.local>
2018-12-09 01:32:26 -0800

    Update location of menu

--- index.php
+++ index.php
@@ -19,40 +19,6 @@
 
 
     <body>
-        <div class="container">
-              <h1>NHL Lines</h1>
-              <p class="lead">Computed NHL Lines calculated based on their ice time together in most recent game</p>
-<?php
-
-// This function reads your DATABASE_URL config var and returns a connection
-// string suitable for pg_connect. Put this in your app.
-function pg_connection_string_from_database_url() {
-    extract(parse_url($_ENV["DATABASE_URL"]));
-    return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-}
-
-function get_team_lines($team_id) {
-    // Establish database connection
-    $pg_conn = pg_connect(pg_connection_string_from_database_url());
-
-    // Now let's use the connection for something silly just to prove it works:
-    $result = pg_query($pg_conn, "SELECT depth,player_id,position FROM lines WHERE team_id=${team_id}");
-
-    $lines = [];
-    while ($row = pg_fetch_row($result)) {
-        $depth = $row[0];
-        $player_id = $row[1];
-        $position = $row[2] == 'D' ? 'D' : 'F';
-
-        $lines[$position][$depth][] = $player_id;
-     }
-
-    return $lines;
-}
-
-$lines = get_team_lines(5);
-
-?>
 
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="container">
@@ -95,8 +61,38 @@ $lines = get_team_lines(5);
             </div><!-- /.navbar-collapse -->
         </div>
 
-
+        <div class="container">
+              <h1>NHL Lines</h1>
+              <p class="lead">Computed NHL Lines calculated based on their ice time together in most recent game</p>
 <?php
+
+// This function reads your DATABASE_URL config var and returns a connection
+// string suitable for pg_connect. Put this in your app.
+function pg_connection_string_from_database_url() {
+    extract(parse_url($_ENV["DATABASE_URL"]));
+    return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+}
+
+function get_team_lines($team_id) {
+    // Establish database connection
+    $pg_conn = pg_connect(pg_connection_string_from_database_url());
+
+    // Now let's use the connection for something silly just to prove it works:
+    $result = pg_query($pg_conn, "SELECT depth,player_id,position FROM lines WHERE team_id=${team_id}");
+
+    $lines = [];
+    while ($row = pg_fetch_row($result)) {
+        $depth = $row[0];
+        $player_id = $row[1];
+        $position = $row[2] == 'D' ? 'D' : 'F';
+
+        $lines[$position][$depth][] = $player_id;
+     }
+
+    return $lines;
+}
+
+$lines = get_team_lines(5);
 
 foreach ($lines as $position => $depths) {
     echo '<table class="table">';
