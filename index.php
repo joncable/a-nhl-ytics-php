371f5e7b8ed2b4ac4c687bf5847cd353f8a58a78
joncable <cablejonathan@gmail.com>
2022-07-18 15:46:47 -0400

    Add LinkedIn locally

--- index.php
+++ index.php
@@ -156,7 +156,7 @@ if (isset($_GET['about'])) {
 ?>
 <div class="container">
     <p class="lead">Passion project to gather and analyze available hockey data which is hosted on <em>Heroku</em> and built using <em>Python</em> and <em>PostgreSQL</em>. Using <em>BeautifulSoup</em> to scrape shift data from the NHL Play-by-Play pages and making requests to available NHL APIs for player metadata, each team's lines are established by grouping players with whom they spent the most time on the ice. Scheduled jobs run frequently to gather new data, calculating and storing the lines in the <em>PostgreSQL</em> database.</p>
-    <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+    <script src="/profile.js"></script>
     <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="dark" data-type="VERTICAL" data-vanity="jonathancable" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://ca.linkedin.com/in/jonathancable?trk=profile-badge"></a></div>
 </div>
 <?php
