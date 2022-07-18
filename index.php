4a74fb9b664232c029907d1c3161c06658d5d5b1
joncable <cablejonathan@gmail.com>
2022-07-18 15:18:56 -0400

    Link to homepage

--- index.php
+++ index.php
@@ -130,7 +130,7 @@ $teams = get_teams();
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li><a href="#">Home</a></li>
+                    <li><a href="/">Home</a></li>
                     <li><a href="?about">About</a></li>
                     <li class="dropdown">
                         <a href="#" data-toggle="dropdown" class="dropdown-toggle">Lines <b class="caret"></b></a>
